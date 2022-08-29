183986f4b7962d163c66fde99a6b9c1234a1d9b0
AkionQuazson <111464265+AkionQuazson@users.noreply.github.com>
2022-08-29 14:52:38 -0600

    Assembled function, tested

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,1 +1,16 @@
 // Write your code below this line.
+
+const luckyNumbers = (numOfNums) => {
+    let numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let chosenNumbers = [];
+
+    for (i = 0; i < numOfNums; i++) {
+        const randomSelection = Math.floor(Math.random() * numberList.length);
+        chosenNumbers.push(numberList.splice(randomSelection, 1))
+    }
+    return chosenNumbers;
+}
+
+console.log("LN4:", luckyNumbers(4));
+console.log("LN7:", luckyNumbers(7));
+console.log("LN2:", luckyNumbers(2));
